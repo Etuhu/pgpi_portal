d2f327786038d662cfdde6f56cec9b22ab688820
Etuhu <andgiz@mail.ru>
2024-04-12 12:03:35 +0700

    Завершена адаптивная верстка главной страницы

--- js/front.js
+++ js/front.js
@@ -38,6 +38,50 @@ Array.prototype.forEach.call(
     })
 );
 
+// (function ($bs) {
+//     $bs.Dropdown.prototype.toggle = function (_orginal) {
+//         return function () {
+//             document.querySelectorAll('.drd-submenu').forEach(function (e) {
+//                 e.classList.remove('drd-submenu');
+//             });
+//             let drd = this._element.closest('.dropdown').parentNode.closest('.dropdown');
+//             for (; drd && drd !== document; drd = drd.parentNode.closest('.dropdown')) {
+//                 drd.classList.add('drd-submenu');
+//             }
+//             return _orginal.call(this);
+//         }
+//     }($bs.Dropdown.prototype.toggle);
+
+//     document.querySelectorAll('.dropdown').forEach(function (drd) {
+//         drd.addEventListener('hide.bs.dropdown', function (e) {
+//             if (this.classList.contains('drd-submenu')) {
+//                 this.classList.remove('drd-submenu');
+//                 e.preventDefault();
+//             }
+//             e.stopPropagation();
+//         });
+//     });
+// })(bootstrap);
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+	//Выпадающее меню при наведении
+	// jQuery(".overlay-menu .dropdown").hover(
+		// function () {
+        //     console.log(jQuery(this).classList[0]);
+		// 	jQuery(this).classList.add("show");
+		// 	jQuery(this).find(".dropdown-menu").classList.add("show");
+		// 	// jQuery(this).find("> .dropdown-menu").show();
+		// 	// jQuery(this).find("> .dropdown-menu").update();
+		// },
+		// function () {
+		// 	jQuery(this).classList.remove("show");
+		// 	jQuery(this).find(".dropdown-menu").classList.remove("show");
+			// jQuery(this).find("> .dropdown-menu").hide();
+			// jQuery(this).find("> .dropdown-menu").update();
+// 		}
+// 	);
+// });
 
 //Подключение кастомного скролла при ширине экрана больше 991 пикселя
 // if ($(window).width() > 991) {
